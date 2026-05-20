5113e82ddc443bb0c783ca3d7023f03a0a01eacd
wrx_365 <93203737+luhihsan@users.noreply.github.com>
2026-05-20 19:48:24 +0700

    fix(client-firebase): secure firebase API key

--- beresin-web/src/app/lib/client.js
+++ beresin-web/src/app/lib/client.js
@@ -1,18 +1,18 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCppYxld9uCw2HTmVxEKFMCTakqT8pu6jc",
-  authDomain: "beres-in-303b0.firebaseapp.com",
-  projectId: "beres-in-303b0",
-  storageBucket: "beres-in-303b0.firebasestorage.app",
-  messagingSenderId: "499766438094",
-  appId: "1:499766438094:web:8802a2e2f80e05489d889f",
-  measurementId: "G-0W87SKMDLF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export { auth, db, app };
